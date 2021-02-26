d51258b7ed661eef24e9b670ab4c0e737e46bde3
hisaotoriyama <hisao.toriyama@gmail.com>
2021-02-26 20:43:32 +0900

    first draft

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="item">
+      <memo-list v-bind:memoLists="memos" v-on:showEditTable="showMemoForm"></memo-list>
+    </div>
+    <div class="item"><button class="btn" v-on:click="addNewMemo">+</button></div>
+
+    <div class="item">
+      <div v-if="show">
+        <memo-form v-bind:editMemoList="editMemo" v-on:registerEdit="processEdit" v-on:registerDelete="processDelete"></memo-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MemoList from './views/MemoList.vue'
+import MemoForm from './views/MemoForm.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    MemoList,
+    MemoForm
+  },
+  data: function() {
+    return {
+      memos:[],
+      show:false,
+      editMemo:{},
+      totalIdx:""
+    }
+  },
+  methods: {
+    loadMemos: function() {
+      const jsonAllMemos = localStorage.getItem('memoList');
+      const parsedJsonAllMemos = JSON.parse(jsonAllMemos);
+      this.totalIdx = parsedJsonAllMemos.length;
+      return parsedJsonAllMemos
+    },
+    setMemosAndReload: function() {
+      localStorage.setItem('memoList', JSON.stringify(this.memos));
+      this.memos = this.loadMemos();
+      location.reload();
+    },
+    showMemoForm: function(eachMemo, idx) {
+      this.show = true;
+      this.editMemo = { memo: eachMemo, idx: idx };
+    },
+    addNewMemo: function() {
+      this.show = true;
+      this.editMemo = { memo: "", idx: this.totalIdx };
+    },
+    processEdit: function(selectedMemo, selectedMemoId) {
+      this.memos[selectedMemoId] = selectedMemo;
+      this.setMemosAndReload();
+      this.show = false;
+    },
+    processDelete: function(selectedMemoId) {
+      this.memos.splice(selectedMemoId,1);
+      this.setMemosAndReload();
+      this.show = false;
+    }
+  },
+  created: function() {
+    this.memos = this.loadMemos();
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  width: 550px;
+  background-color: azure;
+  border: solid;
+  border-radius: 10px;
+  border-color: lightskyblue;
+  padding: 10px;
+}
+
+h1 {
+  width: 500px;
+  font-size: 20px;
+  padding-left: 20px;
+  border-radius: 5px;
+  color: white;
+  background-color: skyblue; 
+}
+.edit-text {
+  margin-left: 20px;
+  color: blue;
+}
+
+.btn {
+  margin-left: 20px;
+  font-size: 15px;
+  color: blue;
 }
 </style>
---
+++ src/views/MemoForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="Edit">
+    <h1>編集</h1>
+    <textarea class="edit-text" placeholder="Memoを入力" v-model="selectedMemo" rows="3" cols="70"></textarea>
+    <button class="btn" v-on:click = "editMemo(selectedMemo, selectedMemoIdx)" >編集</button>
+    <button class="btn" v-on:click = "deleteMemo(selectedMemoIdx)" >削除</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["editMemoList"],
+  data: function() {
+    return {
+      selectedMemo:this.editMemoList.memo,
+      selectedMemoIdx:this.editMemoList.idx
+    }
+  },
+  methods: {
+    editMemo: function(selectedMemo, selectedMemoIdx) {
+      this.$emit("registerEdit", selectedMemo, selectedMemoIdx)
+    },
+    deleteMemo: function(selectedMemo, selectedMemoIdx) {
+      this.$emit("registerDelete", selectedMemo, selectedMemoIdx)
+    },
+  }
+}
+</script>
